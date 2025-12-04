10fd8b600de297eaa95e22497a2bc1da4cb44e08
jnaneshs470-dev <jnaneshs470@gmail.com>
2025-12-04 10:38:13 +0530

    Update Navbar component to increase logo size for improved visibility

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -34,7 +34,7 @@ export function Navbar() {
             <img 
               src="/logo.png" 
               alt="Travunited Logo" 
-              className="h-10 w-auto"
+              className="h-14 w-auto"
             />
           </Link>
 
